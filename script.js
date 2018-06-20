735cbbaf41408cae05fb89a00b539792293035bf
brxtndwg <deji@ogundairo.com>
2018-06-20 01:52:47 +0100

    login validation

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-function validateLogin(thisFrm) {
-    alert(thisFrm.email.value);
-}
+$(document).ready(function(){
+    $( "#login_submit" ).click(function() {
+        msg = "Please Enter Email";
+
+        if (!$( "#loginEmail" ).val()) {
+            $( ".login_email" ).remove();
+            $( "<span class='login_email error'>"+msg+"</span>" ).appendTo( "#login_email" );
+        }
+
+        msg = "Please Enter Password";
+        
+        if (!$( "#loginPassword" ).val()) {
+                $( ".login_password" ).remove();
+            $( "<span class='login_password error'>"+msg+"</span>" ).appendTo( "#login_password" );
+        }
+    });    
+});
